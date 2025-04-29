ae1dff8b0d48bed72fcb9afbf0bb4dce41757d43
launicafisura13 <launicafisura13@gmail.com>
2025-04-29 22:30:18 +0200

    error handling now works on github pages and not just locally

--- script.js
+++ script.js
@@ -32,6 +32,6 @@ document.getElementById('submit').addEventListener('click', () => {
 
     } catch (error) {
         alert(error.message);
-        window.location.href = "index.html";
+        window.location.href = "/gaugeswatch/";
     }
 });
